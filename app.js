745dc1355116784849ff0635e6ca3bb5865c3c7e
Guddu Paul <117582449+Raiden126@users.noreply.github.com>
2023-09-22 23:12:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+}
+const port = 3005;
+
+// define mongoose schema
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+app.use('/static', express.static('static'))
+app.use(express.urlencoded())
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res) => {
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res) => {
+    let myData = new Contact(req.body)
+    myData.save().then(() => {
+        res.send("This item has been send to database")
+    }).catch(() => {
+        res.status(400).send("Item was not saved to the database")
+    });
+
+    // res.status(200).render('contact.pug');
+})
+
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`)
+})
